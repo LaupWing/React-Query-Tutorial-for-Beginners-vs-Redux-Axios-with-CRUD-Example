a48a169224e9ebb2082542bb94a14d1811d0a13d
LaupWing <loc_strict@hotmail.com>
2022-10-29 17:47:31 +0200

    todolist

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,7 +1,10 @@
-import React from "react"
-import { useQuery } from "react-query"
+import React, { useState } from "react"
+import { useQuery, useQueryClient } from "react-query"
 
 const TodoList = () => {
+   const [newTodo, setNewTodo] = useState("")
+   const queryClient = useQueryClient()
+   // const /
    const {} = useQuery()
    return (
       <div>TodoList</div>
