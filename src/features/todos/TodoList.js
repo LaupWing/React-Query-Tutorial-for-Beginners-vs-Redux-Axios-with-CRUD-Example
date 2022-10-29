c585b048606f9b1fddb1cbd3a43a9b472994f347
LaupWing <loc_strict@hotmail.com>
2022-10-29 17:45:59 +0200

    udpate

---
+++ src/features/todos/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { useQuery } from "react-query"
+
+const TodoList = () => {
+   const {} = useQuery()
+   return (
+      <div>TodoList</div>
+   )
+}
+
+export default TodoList
